bdc94e75b9be5ff64f0846308d4a3cdc09f312ce
Andrew Tissi <67088794+TC6IDM@users.noreply.github.com>
2026-06-04 18:56:11 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] newarr = new int[nums.length*2];
+        for (int i=0; i<nums.length; i+=1){
+            System.out.println(nums[i]);
+            newarr[i] = nums[i];
+            newarr[i+nums.length] = nums[i];
+        }
+        return newarr;
+    }
+}
